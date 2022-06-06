b013188d70d47d2f8e3426af1b1cd807ee4c3c1c
Darshan Patel <48265829+nayakpuradarshan@users.noreply.github.com>
2022-06-06 16:43:05 +0530

    Update Alert.py

--- Alert.py
+++ Alert.py
@@ -7,13 +7,20 @@ driver = webdriver.Chrome(executable_path="//Users//darshanpatel//PycharmProject
 validateText = "option3"
 # open URL
 driver.get('https://rahulshettyacademy.com/AutomationPractice/')
+# find element by CSS selector and send details in that field
 driver.find_element_by_css_selector("#name").send_keys(validateText)
+# find element by ID and click on it
 driver.find_element_by_id("alertbtn").click()
 
+# switch to alert from the browser
 alert = driver.switch_to.alert
 
+# store alert text in the alerttext variable
 alerttext = alert.text
+
+# varify that validateText and alerttext is matching
 assert validateText in alerttext
+
 alert.accept()                      # This will click on Ok button in alert
 # alert.dismiss()                     # This will click on the cancel button
 
