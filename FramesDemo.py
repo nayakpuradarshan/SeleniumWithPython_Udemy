aa92653aa545b79ac13f680682cae9410f8eb5b6
Darshan Patel <48265829+nayakpuradarshan@users.noreply.github.com>
2022-06-07 15:26:56 +0530

    Update FramesDemo.py

--- FramesDemo.py
+++ FramesDemo.py
@@ -9,9 +9,12 @@ driver.get('https://the-internet.herokuapp.com/iframe')
 
 # Frame id or frame name or frame value
 driver.switch_to.frame("mce_0_ifr")
+# Clear frame content
 driver.find_element_by_css_selector("#tinymce").clear()
+# Enter some text in the Frame
 driver.find_element_by_css_selector("#tinymce").send_keys("I am able to automate")
 
+# again switch to browser ( From frames )
 driver.switch_to.default_content()
 print(driver.find_element_by_tag_name("h3").text)
 
